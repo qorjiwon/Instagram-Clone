0618a08eb03ae3bbb5bd21a330c2ca3df67bf665
Jiwon Baek <wldnjsl2001@naver.com>
2024-01-16 21:58:19 +0900

    Troubleshooting Sidebar bugs, implementing dark mode

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleElementBtn = document.querySelector('.header__theme-button');
+
+toggleThemeBtn.addEventListener('click', ()=> {
+    document.documentElement.classList.toggle('darkTheme');
+});
