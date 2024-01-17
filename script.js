00aa978ce1a41cec0e12621f21c8f27d70fa1a17
Jiwon Baek <wldnjsl2001@naver.com>
2024-01-17 12:00:13 +0900

    Add Dark Mode

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const toggleElementBtn = document.querySelector('.header__theme-button');
+const toggleThemeBtn = document.querySelector('.header__theme-button');
+
+// Set initial theme from LocalStorage
+document.onload = setInitialTheme(localStorage.getItem('theme'));
+function setInitialTheme(themeKey) {
+    if (themeKey == 'dark') {
+        document.documentElement.classList.add('darkTheme');
+    } else {
+        document.documentElement.classList.remove('darkTheme');
+    }
+}
 
 toggleThemeBtn.addEventListener('click', ()=> {
     document.documentElement.classList.toggle('darkTheme');
-});
+
+    // Saving current theme on LocalStorage
+    if (document.documentElement.classList.contains('darkTheme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+})
